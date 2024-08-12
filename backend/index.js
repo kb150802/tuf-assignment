d690ae36c7d69420dffed159eb9d97244728890f
kb150802 <bansalkushagra150802@gmail.com>
2024-08-12 17:22:48 +0530

    remote db connected

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -7,20 +7,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const pool = mysql.createPool({
-  host: 'localhost',
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST ,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '1234',
   database: 'bannerconfig',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  port: 54817,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the MySQL database as id', connection.threadId);
 });
 
 app.get('/api/banner', async (req, res) => {
   try {
-    const [rows] = await pool.query('SELECT * FROM banner_info LIMIT 1');
-    res.json(rows[0] || { is_visible: false, description: '', timer: 0, link: '' });
+    connection.query('SELECT * FROM banner_info LIMIT 1;',(err, result)=>{
+      if(err) {
+        console.log("Error connecting to the database", err);
+      }
+      console.log(result);
+      res.json(result[0] || { is_visible: false, description: '', timer: 0, link: '' });
+    });
+    
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: 'An error occurred while fetching banner data' });
@@ -30,11 +43,14 @@ app.get('/api/banner', async (req, res) => {
 app.post('/api/banner', async (req, res) => {
   const { is_visible, description, timer, link } = req.body;
   try {
-    await pool.query(
+    connection.query(
       'UPDATE banner_info SET is_visible = ?, description = ?, timer = ?, link = ? WHERE id = 1',
-      [is_visible, description, timer, link]
-    );
-    res.json({ success: true });
+      [is_visible, description, timer, link], (err, result)=> {
+        if(err) {
+          res.status(500).json({error: 'An error occurred while updating banner data'});
+        }
+        res.status(200).json({message: 'Banner Data updated successfuly'});
+      });
   } catch (error) {
     res.status(500).json({ error: 'An error occurred while updating banner data' });
   }
